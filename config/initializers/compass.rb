fe10b289c0b0e5dc0b6377619dc1b53a067b6e3c
Cicero Oliveira <cicero@cicerooliveira.com>
2011-05-28 14:06:37 -0400

    Improved layout.

--- config/initializers/compass.rb
+++ config/initializers/compass.rb
@@ -4,7 +4,7 @@
 #  initializer "create tmp/stylesheets for sass in heroku" do
     FileUtils.mkdir_p(Rails.root.join("tmp", "stylesheets"))
 
-    Compass::AppIntegration::Rails.initialize!
+    #Compass::AppIntegration::Rails.initialize!
 
     Rails.configuration.middleware.delete('Sass::Plugin::Rack')
     Rails.configuration.middleware.insert_before('Rack::Sendfile', 'Sass::Plugin::Rack')
