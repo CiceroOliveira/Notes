ad00f348f29b54c1e23c0d7a4d8363680cc48486
Cicero Oliveira <cicero@cicerooliveira.com>
2011-06-10 00:05:59 -0400

    Added env variables for omniaut: development and production

--- config/heroku_env.rb
+++ config/heroku_env.rb
@@ -1,4 +1,5 @@
 heroku_config = File.expand_path('../heroku.yml', __FILE__)
+
 if File.exists?(heroku_config)
   config = YAML.load_file(heroku_config)
   config.fetch(Rails.env, {}).each do |key, value|
